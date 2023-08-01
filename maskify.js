fabae460611e48433613016c90f5354930e2b59e
wassimxd <karimmohsin10@gmail.com>
2023-08-01 18:04:44 +0100

    Credit Card Mask

---
+++ maskify.js
@@ -0,0 +1,18 @@
+//Credit Card Mask
+/* Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'. */
+
+// return masked string
+function maskify(cc) {
+  let result = "";
+  for (let i = 0; i < cc.length; i++) {
+    if (i < cc.length - 4) {
+      result += "#";
+    } else {
+      result += cc[i];
+    }
+  }
+  return result;
+}
+console.log(maskify("4556364607935616"));
